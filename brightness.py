7341901c4df501e30c3cb388a2be14020aaab67a
Marosg <marosg@gmail.com>
2017-04-21 09:55:59 +0200

    Modifying hard-coded monitor DP1

--- brightness.py
+++ brightness.py
@@ -1,16 +1,19 @@
 import gi
-from gi.repository import Gtk
 gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+import subprocess
 
 
 class Handler:
     def onDeleteWindow(self, *args):
-        print "onDeleteWindow"
+        # print "onDeleteWindow"
         Gtk.main_quit(*args)
 
     def change_value(self, *args):
         self.value = max(min(args[2], 100), 10)
-        print self.value
+        # print self.value
+        subprocess.call("xrandr --output DP1 --brightness {}".
+                        format(self.value/100), shell=True)
 
     def value_change(self, *args):
         pass
